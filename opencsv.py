97b73c7c1304d2f7529dfa04287da75f45f9e034
Hungyun <asteve0929@gmail.com>
2023-11-10 17:10:40 +0800

    深度學習的code在裡面

--- opencsv.py
+++ opencsv.py
@@ -14,3 +14,315 @@ with open('./members.csv', newline='') as f:
     r = csv.DictReader(f)
     for row in r:
         print('{} of {}'.format(row['名字'], row['團體']))
+
+
+
+
+# 以上是原始內容
+
+# 以下是深度學習的code借放一下
+
+import pandas as pd
+import os
+import numpy as np
+try:
+    from sklearn.preprocessing import OrdinalEncoder # just to raise an ImportError if Scikit-Learn < 0.20
+    from sklearn.preprocessing import OneHotEncoder
+except ImportError:
+    from future_encoders import OneHotEncoder # Scikit-Learn < 0.20
+
+try:
+    from sklearn.impute import SimpleImputer # Scikit-Learn 0.20+
+except ImportError:
+    from sklearn.preprocessing import Imputer as SimpleImputer
+
+imputer = SimpleImputer(strategy="median")
+HOUSING_PATH = os.path.join("datasets", "housing")
+csv_path = os.path.join(HOUSING_PATH, "housing.csv")
+
+
+### 最完整原始的資料
+housing = pd.read_csv(csv_path)
+print(housing.info())
+
+### 除掉ocean_proximity的資料
+housing_num = housing.drop('ocean_proximity', axis=1)
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+
+
+
+#Prepare the data for Machine Learning algorithms
+#46
+housing = strat_train_set.drop("median_house_value", axis=1) # drop labels for training set
+housing_labels = strat_train_set["median_house_value"].copy()
+#47
+sample_incomplete_rows = housing[housing.isnull().any(axis=1)].head()
+print(sample_incomplete_rows)
+#48
+
+housing_num = housing.drop('ocean_proximity', axis=1)
+
+
+
+
+#63
+#### Now let's preprocess the categorical input feature, ocean_proximity:
+
+
+housing_cat = housing[["ocean_proximity"]]
+print(housing_cat.head(10))
+
+#64
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+housing_cat_encoded[:10]
+#65
+print(ordinal_encoder.categories_)
+#66
+from sklearn.preprocessing import OneHotEncoder
+
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot
+#67
+housing_cat_1hot.toarray()
+
+#68
+cat_encoder = OneHotEncoder(sparse=False)
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot
+#69
+print(cat_encoder.categories_)
+# 70
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# column index
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+# 71
+housing_extra_attribs = pd.DataFrame(
+    housing_extra_attribs,
+    columns=list(housing.columns)+["rooms_per_household", "population_per_household"],
+    index=housing.index)
+housing_extra_attribs.head()
+# 72
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+print("----------housing_num info----------")
+print(housing_num.info)
+print("housing_num_tr")
+print(housing_num_tr)
+print(housing_num_tr.shape)
+
+# 74
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+
+print("----------housing info----------")
+print(housing.info)
+housing_prepared = full_pipeline.fit_transform(housing)
+
+print("housing_prepared")
+print(housing_prepared)
+print(housing_prepared.shape)
+### housing_prepared為採取補值處理後的訓練測試集
+
+print("--------------housing_labels--------")
+print(housing_labels)
+
+#### Select and train a model
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+# let's try the full preprocessing pipeline on a few training instances
+some_data = housing.iloc[:5]
+# print(some_data)
+some_labels = housing_labels.iloc[:5]
+# print(some_labels)#my
+some_data_prepared = full_pipeline.transform(some_data)
+# print(some_data_prepared)
+print("Linear regression Predictions:", lin_reg.predict(some_data_prepared))
+
+
+
+# Compare against the actual values:!
+print("Linear regression Labels:", list(some_labels))
+print(some_data_prepared)
+
+
+# evaluate model performance
+from sklearn.metrics import mean_squared_error
+
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+# 這個值應該是越小越好
+print(lin_rmse)
+
+from sklearn.metrics import mean_absolute_error
+
+lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+print(lin_mae)
+
+# 改用決策樹回歸
+from sklearn.tree import DecisionTreeRegressor
+
+tree_reg = DecisionTreeRegressor(random_state=42)
+tree_reg.fit(housing_prepared, housing_labels)
+
+# let's try the full preprocessing pipeline on a few training instances
+some_data = housing.iloc[:5]
+# print(some_data)
+some_labels = housing_labels.iloc[:5]
+# print(some_labels)#my
+some_data_prepared = full_pipeline.transform(some_data)
+# print(some_data_prepared)
+print("Decision tree Predictions:", tree_reg.predict(some_data_prepared))
+
+# Compare against the actual values:!
+print("Decision tree Labels:", list(some_labels))
+
+
+
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+print(tree_rmse)
+###結果為0 代表此model是 over fitting
+
+
+
+########     Fine-tune your model       ######
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+                         scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+
+### 驗證決策樹得分
+display_scores(tree_rmse_scores)
+
+lin_scores = cross_val_score(lin_reg, housing_prepared, housing_labels,
+                             scoring="neg_mean_squared_error", cv=10)
+lin_rmse_scores = np.sqrt(-lin_scores)
+### 驗證線性回歸得分
+display_scores(lin_rmse_scores)
+# 由這邊的結果才能看出，線性回歸表現比決策樹更好
+#Note: we specify n_estimators=100 to be future-proof since the default value is going to change to 100 in Scikit-Learn 0.22 (for simplicity, this is not shown in the book).
+# 這句話看不太懂
+
+
+### 換隨機森林試試看
+from sklearn.ensemble import RandomForestRegressor
+
+forest_reg = RandomForestRegressor(n_estimators=100, random_state=42)
+forest_reg.fit(housing_prepared, housing_labels)
+
+housing_predictions = forest_reg.predict(housing_prepared)
+forest_mse = mean_squared_error(housing_labels, housing_predictions)
+forest_rmse = np.sqrt(forest_mse)
+print(forest_rmse)
+
+
+
+from sklearn.model_selection import cross_val_score
+
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+### 驗證隨機森林得分
+display_scores(forest_rmse_scores)
+
+scores = cross_val_score(lin_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+print(pd.Series(np.sqrt(-scores)).describe())
+
+### 支持向量機
+from sklearn.svm import SVR
+
+svm_reg = SVR(kernel="linear")
+svm_reg.fit(housing_prepared, housing_labels)
+housing_predictions = svm_reg.predict(housing_prepared)
+svm_mse = mean_squared_error(housing_labels, housing_predictions)
+svm_rmse = np.sqrt(svm_mse)
+print(svm_rmse)
+
+
+# 以下就直接採用random forest
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+
+forest_reg = RandomForestRegressor(random_state=42)
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training 
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_mean_squared_error',
+                           return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
+
+print(grid_search.best_params_)
+
+print(grid_search.best_estimator_)
+
+cvres = grid_search.cv_results_
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params)
